0629bf607861fc2079ed0efa6a4500d9791b42d1
ShiroTsuma-github <spam.shiro@gmail.com>
2024-03-08 16:44:20 +0100

    another huge progress. search bar basically finished

--- packages/product.py
+++ packages/product.py
@@ -1,17 +1,66 @@
 from packages import app
 from flask import render_template
 import os
+from random import choice
 import json
 app.config["DEBUG"] = True
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)).replace("packages", "")
 
-# https://www.geeksforgeeks.org/how-to-build-a-web-app-using-flask-and-sqlite-in-python/
 
-# w formie komentarz
+userData = [
+    {
+        'name': 'ShiroTsuma',
+        'messages': 11,
+        'coins': 265,
+        'lvl_offset': [0, -21]
+    },
+    {
+        'name': 'Hubinix',
+        'messages': 23,
+        'coins': 745,
+        'lvl_offset': [0, -337]
+    },
+    {
+        'name': 'Dawid Nakrewicz',
+        'messages': 1,
+        'coins': 10,
+        'lvl_offset': [0, 0]
+    },
+    {
+        'name': 'unlogged',
+        'messages': 0,
+        'coins': 0,
+        'lvl_offset': [0, 0]
+    }
+]
+leagues = ['Hardcore Affliction', 'Ruthless Affliction', 'HC Ruthless Affliction',
+           'Standard', 'Hardcore', 'Ruthless', 'Hardcore Ruthless']
+currencies = [['Awakened Sextant', 'images/sextant_awakened.png'], ['Ancient Orb', 'images/orb_ancient.png'],
+              ['Orb of Annulment', 'images/orb_annulment.png'], ['Orb of Binding', 'images/orb_binding.png'], ['Divine Orb', 'images/orb_divine.png'], ['Elevated Sextant', 'images/sextant_elevated.png']]
 items = json.loads(open(ROOT_DIR + "data/items.json").read())
+prices = {
+    'Chaos Orb': 1,
+    'Ancient Orb': 5,
+    'Orb of Annulment': 15,
+    'Orb of Binding': 20,
+    'Divine Orb': 150,
+    'Awakened Sextant': 2,
+    'Elevated Sextant': 200
+}
+iter_ = -1
+
 
 @app.route('/')
 def index():
-    return render_template("base.html", items=items)
-
-
+    global iter_
+    return render_template("base.html",
+                           items=items,
+                           userdata=userData[0],
+                           leagues=leagues,
+                           currencies=currencies)
+    iter_ += 1
+    # return render_template("base.html",
+    #                        items=items,
+    #                        userdata=userData[iter_ % 4],
+    #                        leagues=leagues,
+    #                        currencies=currencies)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -12,13 +12,19 @@ window.addEventListener("keypress", function(e){
 });  
 document.addEventListener("DOMContentLoaded", function() {
   // Get the input element with class "multiselect__input"
-  const inputElement = document.querySelector(".multiselect__input");
+  const inputElement = document.querySelector("#item-input");
+  const input_container = document.querySelector("#search-container");
 
   // Get the element with class "multiselect__content-wrapper"
-  const element = document.querySelector(".multiselect__content-wrapper");
+  const name_search_element = document.querySelector("#item-list");
+
+  const selectedOption = document.querySelector('#league-container');
+  const dropdownContent = document.querySelector('#league-options');
 
   const elementsList = document.querySelector(".multiselect__content");
 
+  const currency_container = document.querySelector('#currency-container');
+  const currency_options = document.querySelector('#currency-options');
   // Get the "No elements found" message element
   const notFoundMessage = document.getElementById("item-not-found");
 
@@ -26,14 +32,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
   // Check if the input element and the content element are found
-  if (inputElement && element && elementsList && notFoundMessage && clearButton) {
+  if (inputElement && name_search_element && elementsList && notFoundMessage && clearButton && selectedOption && dropdownContent) {
     // Add input event listener to the input element
     inputElement.addEventListener("input", function() {
         // Get the search query from the input field
         const searchQuery = inputElement.value.trim().toLowerCase();
-
         // Flag to track if any matching elements are found
         let matchFound = false;
+
+        input_container.classList.add('multiselect--active');
 
         // Iterate over each element in the list
         elementsList.querySelectorAll(".multiselect__element").forEach(function(li) {
@@ -61,7 +68,8 @@ document.addEventListener("DOMContentLoaded", function() {
             inputElement.value = text;
             // Trigger input event to update the search results
             inputElement.dispatchEvent(new Event("input"));
-            element.style.display = "none";
+            name_search_element.style.display = "none";
+            input_container.classList.remove('multiselect--active');
         });
       });
 
@@ -75,20 +83,97 @@ document.addEventListener("DOMContentLoaded", function() {
         // Set the display property of the content wrapper based on search query
         if (searchQuery !== "") {
             // Set the display property to make it visible
-            element.style.display = "block"; // or "inline", "flex", etc. depending on your layout needs
+            name_search_element.style.display = "block"; // or "inline", "flex", etc. depending on your layout needs
         } else {
             // Set the display property to hide it
-            element.style.display = "none";
+            name_search_element.style.display = "none";
+            input_container.classList.remove('multiselect--active');
         }
 
-    
+    });
+    // Add event listener to the selected option
+    selectedOption.addEventListener('click', function() {
+      // Toggle the visibility of the dropdown content
+      dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
+      if (dropdownContent.style.display === "block") {
+        selectedOption.classList.add('multiselect--active');
+      }
+      else {
+        selectedOption.classList.remove('multiselect--active');
+      }
+
+    });
+    let options = dropdownContent.querySelectorAll('.multiselect__element');
+
+    options.forEach(function(option) {
+        option.addEventListener('click', function() {
+            options.forEach(function(opt) {
+              opt.querySelector('.multiselect__option').classList.remove('multiselect__option--selected');
+          });
+          option.querySelector('.multiselect__option').classList.add('multiselect__option--selected');
+            // Replace the text of the selected option
+            let temp = document.querySelector('#leagueText');
+            temp.textContent = option.textContent;
+        
+            
+            // Hide the dropdown content
+            dropdownContent.classList.remove('show');
+        });
+    });
+    currency_container.addEventListener('click', function() {
+      // Toggle the visibility of the dropdown content
+      currency_options.style.display = currency_options.style.display === "block" ? "none" : "block";
+      if (currency_options.style.display === "block") {
+        currency_container.classList.add('multiselect--active');
+      }
+      else {
+        currency_container.classList.remove('multiselect--active');
+      }
+
+    });
+    let curr_options = currency_options.querySelectorAll('.multiselect__element');
+
+    curr_options.forEach(function(option) {
+        option.addEventListener('click', function() {
+            curr_options.forEach(function(opt) {
+              opt.querySelector('.multiselect__option').classList.remove('multiselect__option--selected');
+          });
+          option.querySelector('.multiselect__option').classList.add('multiselect__option--selected');
+            // Replace the text of the selected option
+            let temp = document.querySelector('#select-currency');
+            temp.innerHTML = option.children[0].innerHTML;
+        
+            
+            // Hide the dropdown content
+            dropdownContent.classList.remove('show');
+        });
     });
     clearButton.addEventListener("click", function() {
       // Clear the input field
       inputElement.value = "";
+      let options = dropdownContent.querySelectorAll('.multiselect__option');
+      options.forEach(function(opt) {
+        opt.classList.remove('multiselect__option--selected');
+      });
+      let temp = document.querySelector('.search-right .multiselect.status-select .multiselect__single');
+            temp.textContent = options[0].textContent;
+            options[0].classList.add('multiselect__option--selected');
 
+      let minInput = document.querySelector('.search-right .filter input[placeholder="min price"]');
+      let maxInput = document.querySelector('.search-right .filter input[placeholder="max price"]');
+      minInput.value = "";
+      maxInput.value = "";
+      
       // Trigger input event to update the search results
       inputElement.dispatchEvent(new Event("input"));
+      input_container.classList.remove('multiselect--active');
+
+      let curr_options = currency_options.querySelectorAll('.multiselect__element');
+      curr_options.forEach(function(opt) {
+        opt.querySelector('.multiselect__option').classList.remove('multiselect__option--selected');
+      });
+      curr_options[0].querySelector('.multiselect__option').classList.add('multiselect__option--selected');
+      document.querySelector('#select-currency').innerHTML = curr_options[0].children[0].innerHTML;
   });
 } else {
     console.log("Input element, content wrapper element, elements list, or not found message not found.");
